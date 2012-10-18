46fb34e2cecb1327e74e709073d7d1d67a19a8d8
Patrick E. Burke <uconnfootball75@yahoo.com>
2012-10-18 17:03:38 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studybug::Application.config.secret_token = 'dfd546887ed72d627cd1f278dbe3e9d2c36a333006cd918e73475a0c7749f567b8d9e199fd01c576769cc47e08ab38dbaa7a2f257848f2dc313cf8bca47ee327'
